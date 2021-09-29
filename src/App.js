5d204442ab3d6082f64d16403898fa9d717f81a6
Beatriz Motta <bigobaldohoran@gmail.com>
2021-09-29 15:47:06 -0300

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
+import {AuthContext} from './contexts/AuthContext'
+import {BrowserRouter as Router, Route, Link, Switch, useHistory} from 'react-router-dom'
+import Landing from './pages/Landing'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Profile from './pages/Profile'
+import '../node_modules/font-awesome/css/font-awesome.css'
+import axios from 'axios';
+import Admin from './pages/Admin';
+import AdminProfile from './pages/AdminProfile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [auth, setAuth] = useState()
+    const [userData, setUserData] = useState() 
+
+    axios.defaults.withCredentials = true 
+
+    useEffect(() => {
+        axios.get('http://localhost:9091/login')
+            .then((res) => {
+                console.log(res.data)
+                setUserData(res.data)
+            })
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{authConfig: [auth, setAuth], user: [userData, setUserData]}}>
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/cadastro" component={Signup}/>
+                    <Route exact path="/profile" component={Profile}/>
+                    <Route exact path="/admin" component={Admin} />
+                    <Route exact path="/admin/profile" component={AdminProfile} />
+                </Switch>
+            </Router>
+        </div>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
